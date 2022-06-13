6da676ca16bccee810730875636d669e30375fc9
giu1s <djgiulia04@gmail.com>
2022-06-13 13:11:56 +0200

    addes api to url

--- src/main/java/com/sports/plan/controller/UserController.java
+++ src/main/java/com/sports/plan/controller/UserController.java
@@ -16,10 +16,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
+@RequestMapping(value = "/api")
 public class UserController {
     
     @Autowired
@@ -31,7 +33,7 @@ public class UserController {
     /**
      * @return all user
      */
-    @GetMapping("/api/allusers")
+    @GetMapping("/allusers")
     public List<User> getUser(){
         return userRepository.findAll();
     }
